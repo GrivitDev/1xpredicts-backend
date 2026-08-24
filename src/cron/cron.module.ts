ae72606218a2d3e6d703875b9603438e7aad04c4
GrivitDev <grivitdev@gmail.com>
2026-08-24 12:24:13 +0100

    commit

--- src/cron/cron.module.ts
+++ src/cron/cron.module.ts
@@ -1,27 +1,14 @@
 import { Module } from '@nestjs/common';
 
-import { ScheduleModule } from '@nestjs/schedule';
-
 import { CronService } from './cron.service';
-
 import { SettlementCronService } from './settlement.cron.service';
 
 import { UsersModule } from '../users/users.module';
-
 import { PaymentsModule } from '../payments/payments.module';
-
 import { PredictionsModule } from '../predictions/predictions.module';
 
 @Module({
-  imports: [
-    ScheduleModule.forRoot(),
-
-    UsersModule,
-
-    PaymentsModule,
-
-    PredictionsModule,
-  ],
+  imports: [UsersModule, PaymentsModule, PredictionsModule],
 
   providers: [CronService, SettlementCronService],
 })
